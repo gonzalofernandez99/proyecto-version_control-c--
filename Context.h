72cb7c7db476a5beade2228965fc72321da00abc
gonzalofernandez99 <fernandezgonzalo55@gmail.com>
2023-06-18 18:34:03 -0300

    Context

--- Context.h
+++ Context.h
@@ -7,19 +7,40 @@ using UUser::User;
 using UDateTime::DateTime;
 
 namespace UContext{
+	static User* currentUser = nullptr;
+	static DateTime* currentDateTime = nullptr;
 	// Precondicion: Ninguna
 	// Postcondicion: Obtiene el usuario actual.
 	// Para la entrega actual debe crear un unico usuario
 	// y siempre debe devolver esa misma instancia de usuario
-	User* GetCurrentUser();
+	User* GetCurrentUser(){
+		if(currentUser == nullptr){
+            currentUser = UUser::CreateUser("Default User");
+        }
+        return currentUser;
+	}
 
 	// Precondicion: Ninguna
 	// Postcondicion: Libera todos los recursos asociados al contexto 
-	DateTime* GetNow();
+	DateTime* GetNow(){
+		if(currentDateTime == nullptr){
+            currentDateTime = UDateTime::Now();
+        }
+        return currentDateTime;
+	}
 
 	// Precondicion: Ninguna
 	// Postcondicion: Libera todos los recursos asociados al contexto
-	void DestroyContext();
+	void DestroyContext(){
+		if(currentUser != nullptr){
+            UUser::DestroyUser(currentUser);
+            currentUser = nullptr;
+        }
+        if(currentDateTime != nullptr){
+            UDateTime::DestroyDateTime(currentDateTime);
+            currentDateTime = nullptr;
+        }
+	}
 }
 
 #endif
